65a949e7ee67b82504aba2f4bbf8874de79c8473
HuangYi <huang@crypto.com>
2022-10-18 16:44:40 +0800

    add flake app

--- flake.nix
+++ flake.nix
@@ -10,32 +10,34 @@
 
   outputs = { self, nixpkgs, flake-utils, rocksdb-src }:
     let
-      package-env = { poetry2nix, lib, rocksdb }:
+      overrides = { poetry2nix, rocksdb, lib }: poetry2nix.overrides.withDefaults
+        (lib.composeManyExtensions [
+          (self: super:
+            let
+              buildSystems = {
+                rocksdb = [ "setuptools" "cython" "pkgconfig" ];
+                pyroaring = [ "setuptools" ];
+                roaring64 = [ "poetry" ];
+                cprotobuf = [ "setuptools" ];
+              };
+            in
+            lib.mapAttrs
+              (attr: systems: super.${attr}.overridePythonAttrs
+                (old: {
+                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: self.${a}) systems;
+                }))
+              buildSystems
+          )
+          (self: super: {
+            rocksdb = super.rocksdb.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ rocksdb ];
+            });
+          })
+        ]);
+      versiondb-env = { poetry2nix, lib, rocksdb }:
         poetry2nix.mkPoetryEnv {
           projectDir = ./.;
-          overrides = poetry2nix.overrides.withDefaults (lib.composeManyExtensions [
-            (self: super:
-              let
-                buildSystems = {
-                  rocksdb = [ "setuptools" "cython" "pkgconfig" ];
-                  pyroaring = [ "setuptools" ];
-                  roaring64 = [ "poetry" ];
-                  cprotobuf = [ "setuptools" ];
-                };
-              in
-              lib.mapAttrs
-                (attr: systems: super.${attr}.overridePythonAttrs
-                  (old: {
-                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: self.${a}) systems;
-                  }))
-                buildSystems
-            )
-            (self: super: {
-              rocksdb = super.rocksdb.overridePythonAttrs (old: {
-                buildInputs = (old.buildInputs or [ ]) ++ [ rocksdb ];
-              });
-            })
-          ]);
+          overrides = overrides { inherit poetry2nix lib rocksdb; };
         };
     in
     (flake-utils.lib.eachDefaultSystem
@@ -51,16 +53,28 @@
         in
         rec {
           packages = {
-            dev-env = pkgs.callPackage package-env { };
+            versiondb-env = pkgs.callPackage versiondb-env { };
+            versiondb-cli = pkgs.writeShellScriptBin "versiondb" ''
+              ${packages.versiondb-env}/bin/versiondb $@
+            '';
+          };
+          defaultPackage = packages.versiondb-cli;
+          apps = {
+            default = {
+              type = "app";
+              program = "${packages.versiondb-cli}/bin/versiondb";
+            };
           };
           devShell = pkgs.mkShell {
-            buildInputs = [ packages.dev-env ];
+            buildInputs = [ packages.versiondb-env ];
           };
         }
       )
     ) // {
       overlay = final: prev: {
-        rocksdb = (prev.rocksdb.override { enableJemalloc = true; }).overrideAttrs (old: rec {
+        rocksdb = (prev.rocksdb.override {
+          enableJemalloc = true;
+        }).overrideAttrs (old: rec {
           pname = "rocksdb";
           version = "6.29.5";
           src = rocksdb-src;
